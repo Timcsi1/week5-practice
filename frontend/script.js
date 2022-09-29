be68febb8c1dd39f909dfeaea99cb25a06205871
Timi <timeaher@gmail.com>
2022-09-29 10:43:58 +0200

    AddEventListener

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,81 @@
+const year =[{
+    "month": "January",
+    "id": "jan",
+    "nth": 1,
+    "days": 31,
+},
+{
+    "month": "February",
+    "id": "febr",
+    "nth": 2,
+    "days": 28,
+},
+{
+    "month": "March",
+    "id": "marc",
+    "nth": 3,
+    "days": 31,
+},
+{
+    "month": "April",
+    "id": "apr",
+    "nth": 4,
+    "days": 30,
+},
+{
+    "month": "May",
+    "id": "may",
+    "nth": 5,
+    "days": 31,
+},
+{
+    "month": "June",
+    "id": "june",
+    "nth": 6,
+    "days": 30,
+},
+{
+    "month": "July",
+    "id": "july",
+    "nth": 7,
+    "days": 31,
+},
+{
+    "month": "August",
+    "id": "aug",
+    "nth": 8,
+    "days": 31,
+},
+{
+    "month": "September",
+    "id": "sept",
+    "nth": 9,
+    "days": 30,
+},
+{
+    "month": "October",
+    "id": "oct",
+    "nth": 10,
+    "days": 31,
+},
+{
+    "month": "November",
+    "id": "nov",
+    "nth": 11,
+    "days": 30,
+},
+{
+    "month": "December",
+    "id": "dec",
+    "nth": 12,
+    "days": 31,
+}
+];
+
 const monthComponent = function(nth, name, days){
     let daysHTML="";
 
-    for(let i=1; i<=31; i++) {
+    for(let i=1; i<=days; i++) {
         daysHTML+=dayComponent(i);
     }
 
@@ -18,15 +92,27 @@ const dayComponent = function(dayCount)
 return`
 <div class="day">${dayCount}</div>`}
 
-console.log("hello")
+console.log("hello");
+console.log(year[0])
 const rootElement =document.querySelector("#root");
 
-// rootElement.insertAdjacentHTML("beforeend","<button>show calendar</button>");
-// const buttonElement = rootElement.querySelector("button");
-// buttonElement.addEventListener("click",function(){
+rootElement.insertAdjacentHTML("beforeend","<button>add month</button>");
+const buttonElement = rootElement.querySelector("button");
 
-//}) gomb, annyiszor betölti, ahányszor megnyomjuk
+let monthIndex= 0;
 
-rootElement.insertAdjacentHTML("beforeend",monthComponent(1, "January", 31));
-rootElement.insertAdjacentHTML("beforeend",monthComponent(2, "February", 28));
+buttonElement.addEventListener("click",function(){
+    if(monthIndex<12){ 
+        rootElement.insertAdjacentHTML("beforeend",monthComponent(year[monthIndex].nth, year[monthIndex].month, year[monthIndex].days));
+        console.log(year[monthIndex])
+        monthIndex++;
+    }else{
+        buttonElement.setAttribute("disabled","");
+    }
+})
+
+
+//rootElement.insertAdjacentHTML("beforeend",monthComponent(year[monthIndex].nth, year[monthIndex].month, year[monthIndex].days));
+
+//rootElement.insertAdjacentHTML("beforeend",monthComponent(2, "February", 28));
 //rootElement.style.color="blue" nem szabad csinálni, csak CSS-ben
